0ff378157124e1e3201831135458a69649bdc9be
Eduardo Viana <eduardo.c.viana@hotmail.com>
2025-04-27 20:18:45 -0300

    Versão inicial com rotas do projeto Cloud

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Bem-vindo ao Projeto Cloud 2025!</h1>");
+});
+
+app.get("/contato", (req, res) => {
+    res.send("<h1>Página de contato: fale conosco.</h1>");
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("<h1>Sobre: projeto de deploy em nuvem.</h1>");
+});
+
+app.get("/cliente/:id", (req, res) => {
+    res.send("Cliente ID: " + req.params.id);
+});
+
+app.get("/produto/:nome", (req, res) => {
+    const nome = req.params.nome;
+    if (nome) {
+        res.send("Produto: " + nome);
+    } else {
+        res.send("Nenhum produto informado.");
+    }
+});
+
+app.listen(process.env.PORT ?? 3000, () => {
+    console.log("Servidor rodando na porta 3000.");
+});
